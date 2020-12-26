31e260d591398bd344817235092c2230c8acf033
aditya_gangasagar <adig452@gmail.com>
2020-12-26 23:14:19 +0530

    Initial Comment

---
+++ app/src/androidTest/java/com/jai_guru_dev/aditya_gangasagar/calculatorkotlin/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.jai_guru_dev.aditya_gangasagar.calculatorkotlin
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.cropdata.toolsuite.calculatorkotlin", appContext.packageName)
+    }
+}
